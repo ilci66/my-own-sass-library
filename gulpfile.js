8593110a5823c15119eaaa1d801c6402e02dd726
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-28 15:10:30 +0300

    using impartials

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 // pretty obvious what's going on here
 function buildStyles() {
-  return src('index.scss')
+  return src('*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 // watching for changes and calling buildStyles
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(['*.scss'], buildStyles)
 }
 
 // these will be ran in turn, first buildStyles then watchTask
