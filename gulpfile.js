5eea09c317c84d5ae708b1aaca1da20bb7a4f7dd
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-30 11:33:34 +0300

    purged unused & gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-  // ** means any subfolders too, so it's gonna check the subfolders for scss files
-  return src('shinobi/**/*.scss')
-    .pipe(sass())
+  return src('shinobi/**/*.scss')   
+    // .pipe(sass()) // wanted to compress
+    .pipe(sass({ outputStyle: 'compressed' }))
+    // to show what css values it should render, it can be an array of paths relative
+    // to this file, the way I did it it will look for all the root level html files 
+    .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['shinobi/**/*.scss'], buildStyles)
+  // wanna watch for changes in the html pages too
+  watch(['shinobi/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
